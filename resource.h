8d68875b18db5ff1d59d1f28c38a6b44f5e418bc
v <v@DESKTOP-NB7V0HL>
2025-01-15 09:02:00 -0800

    Add project files.

---
+++ resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by test1.rc
+//
+#define IDC_MYICON                      2
+#define IDD_TEST1_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_TEST1                       107
+#define IDI_SMALL                       108
+#define IDC_TEST1                       109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       129
+#define IDR_MENU1                       131
+#define IDR_ACCELERATOR1                132
+#define IDR_ACCELERATOR2                133
+#define IDM_CHANGLE_ENGLISH             201
+#define IDM_CHANGLE_VIETNAMEESE         202
+#define ID_HELP_ENGLISH                 32771
+#define ID_HELP_VIETNAMESE              32772
+#define ID_FILE_CLOSE                   32773
+#define ID_FILE_NEW                     32777
+#define ID_FILE_OPEN                    32778
+#define ID_LANGUAGES_ENGLISH            32779
+#define ID_LANGUAGES_VIETNAMESE         32780
+#define ID_THUMUC_MO                    32781
+#define ID_THUMUC_DONG                  32782
+#define ID_THUMUC_TAOMOI                32783
+#define ID_NGONNGU_TIENGANH             32784
+#define ID_NGONNGU_TIENGVIET            32785
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32795
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
